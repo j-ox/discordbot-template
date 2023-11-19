31aa70380643b824e2dba95d31fa26a96119fc75
J ️ <j_ox@icloud.com>
2023-11-18 22:41:53 -0500

    clear.js

---
+++ utils/clear.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'clear',
+    description: 'Deletes an X ammount of messages',
+    async execute(message, args) {
+        
+            const deleteCount = parseInt(args[0], 10);
+            if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+                return message.reply('Please provide a number between 2 and 100 for the number of messages to delete');
+            const fetched = await message.channel.messages.fetch({ limit: deleteCount });
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+        }
+
+    };
