794fc0c370e0ceb6476d869294c2e32237e4e9da
J ️ <j_ox@icloud.com>
2023-11-19 01:25:50 -0500

    ping op

--- dev/ping.js
+++ dev/ping.js
@@ -1,22 +1,20 @@
-const {EmbedBuilder} = require('discord.js');
+const {EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'ping',
     description: 'Calculates API and message latency',
-    execute(message, args) {
-      message.channel.send('Calculating ping...').then(sentMessage => {
-        const apiLatency = Math.round(message.client.ws.ping);
-        const messageLatency = sentMessage.createdTimestamp - message.createdTimestamp;
-        const embed = new EmbedBuilder()
-        .setDescription(`**Ping\n** API Latency , ${apiLatency}ms\nMessage Latency, ${messageLatency}ms`)
-        .setColor('#00ff00');
-  
-      sentMessage.edit({ content: '', embeds: [embed] })
-        .catch((error) => {
-          console.error('Error editing message:', error);
-        });
-       
-      });
+    async execute(message) {
+        try {
+            const sentMessage = await message.channel.send('Calculating ping...');
+            const apiLatency = Math.round(message.client.ws.ping);
+            const messageLatency = sentMessage.createdTimestamp - message.createdTimestamp;
+            const embed = new EmbedBuilder()
+                .setDescription(`**Ping**\nAPI Latency: ${apiLatency}ms\nMessage Latency: ${messageLatency}ms`)
+                .setColor('#00ff00');
+
+            await sentMessage.edit({ content: '', embeds: [embed] });
+        } catch (error) {
+            console.error('Error editing message:', error);
+        }
     }
-  };
-
-
+};
