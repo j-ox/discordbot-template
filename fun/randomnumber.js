dd7998d60dfe516a89c4ad32c702cbc975438a70
J ️ <j_ox@icloud.com>
2023-11-19 01:46:38 -0500

    added an embed to rn looks cleaner

--- fun/randomnumber.js
+++ fun/randomnumber.js
@@ -1,11 +1,27 @@
+const {EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: 'rn',
-    description: 'it will print a random number',
+    description: 'Prints a random number',
     execute(message, args) {
-
-// Commands goes here you retard
-  const randomNumber = Math.floor(Math.random() * 100);     
-message.channel.send(`Random Number : ${randomNumber}` );
-    
-  }
+      const color = getRandomColor();
+        const randomNumber = Math.floor(Math.random() * 100);
+        
+        const embed = new EmbedBuilder()
+            .setTitle('Random Number')
+            .setDescription(`Random Number: ${randomNumber}`)
+            .setColor(color);
+        
+        message.channel.send({ content: '', embeds: [embed] });
+    }
 };
+
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
