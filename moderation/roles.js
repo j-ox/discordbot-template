26e90a69eec0be8a61acc5dc7d229315c67c34ed
J ️ <j_ox@icloud.com>
2023-11-20 20:16:35 -0500

    added th roles cmnd to mod folder and fixed

---
+++ moderation/roles.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'roles',
+    description: 'Displays all roles in the server',
+    async execute(message) {
+        const roles = message.guild.roles.cache.map(role => `<@&${role.id}>`).join(", ");
+
+        const embed = new EmbedBuilder()
+            .setDescription(`Roles: ${roles}`)
+            .setColor(getRandomColor());
+
+        message.channel.send({ content: 'Here are the roles:', embeds: [embed] });
+    }
+};
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
